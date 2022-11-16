3c3c26672ed03f2c93199b6fcda78793740bd327
victorefe <victor.bolivar@pucp.edu.pe>
2022-11-15 21:42:06 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 $(document).ready(function (){
-  $("#botonSubirfotos").click(function (){
-    var archivo = document.getElementById('input').files[0];
-    var reader = new FileReader();
-
-    if (file) {
-      reader.readAsDataURL(archivo);
-      reader.onloadend = function () {
-        document.getElementById("imgShow").src = reader.result;
+  $(document).on("change", "#cargarFoto",
+    function (){
+      console.log(this.files);
+      var files= this.files;
+      var element;
+      var supportedImages= ["image/png"];
+      //Máximo 5 fotos
+      if(files.length<=5){
+        for(let i=0;i<files.length; i++){
+          element= files[i];
+          if(supportedImages.indexOf(element.type)!=-1) {
+            createPreview(element, i);
+            console.log("se creó");
+          }
+        }
+        $('#cantidadImg').empty();
+        $('#cantidadImg').append('Cantidad de imágenes: '+files.length);
+        //Aquí empezamos la creación de un nuevo input file
+        /*var newInputFile= createInputFile();
+        $("#add-photo-conteiner").append(newInputFile);*/
       }
-    }
-  });
+    });
 });
+
+function createPreview(file, index){
+  var imgCodified= URL.createObjectURL(file);
+  var name= file.name;
+  //var img=$('<div id="img"><img style=" width: 132px;height: 100px;" src="'+imgCodified+'"/></div>')
+
+  var img=$('<div id="img"><img style=" width: 132px;height: 100px;" src="https://www.egames.news/__export/1617562106480/sites/debate/img/2021/04/04/pokemon-go_x6x.jpg_172596871.jpg"/></div>')
+  $('#imagenes0').append(img);
+}
